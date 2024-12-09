7e1ea4f3225fbd1bc7cda7c30db22e2b7ef72d8c
artemlisenkov <luganluganlugan@gmail.com>
2024-12-09 01:48:52 +0100

    feat: add month navigation and dynamic month/year updates in calendar

--- CalendarTracker/Managers/DateManager.swift
+++ CalendarTracker/Managers/DateManager.swift
@@ -2,16 +2,16 @@ import Foundation
 
 
 struct DateManager {
-    static func generateDatesForMonth() throws -> [Date] {
-        let calendar = Calendar.current
-        let today = Date()
-        
-        guard let range = calendar.range(of: .day, in: .month, for: today) else {
+    private static let calendar = Calendar.current
+
+    
+    static func generateDatesForMonth(for date: Date) throws -> [Date] {
+        guard let range = calendar.range(of: .day, in: .month, for: date) else {
             throw CalendarError.emptyDatesArray
         }
         
         let dates = range.compactMap { day -> Date? in
-            var components = calendar.dateComponents([.year, .month], from: today)
+            var components = calendar.dateComponents([.year, .month], from: date)
             components.day = day
             
             return calendar.date(from: components)
@@ -25,6 +25,17 @@ struct DateManager {
     }
     
     
+    static func changeDateByMonth(by offset: Int, from date: Date) throws -> Date? {
+        guard let changedDate = calendar.date(byAdding: .month, value: offset, to: date)
+        else {
+            throw CalendarError.invalidDate
+
+        }
+        
+        return changedDate
+    }
+    
+    
 //    static func generateDatesForOffsetMonth(for date: Date, by offset: Int) throws -> [Date] {
 //        guard let nextMonth = Calendar.current.date(byAdding: .month, value: offset, to: date) else {
 //            throw CalendarError.invalidDate
@@ -34,11 +45,11 @@ struct DateManager {
 //    }
     
     
-    static func retrieveMonthAndYear() -> String {
+    static func retrieveMonthAndYear(from date: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "MMMM, yyyy"
         
-        return formatter.string(from: Date())
+        return formatter.string(from: date)
     }
     
     
--- CalendarTracker/ViewModels/CalendarViewModel.swift
+++ CalendarTracker/ViewModels/CalendarViewModel.swift
@@ -3,17 +3,44 @@ import SwiftUI
 class CalendarViewModel: ObservableObject {
     @Published var days: [Date] = []
     @Published var selectedDay: Date = Date()
+    @Published var dateForGenerating: Date = Date()
     @Published var errorMessage: String? = nil
+    @Published var monthAndYear: String = DateManager.retrieveMonthAndYear(from: Date())
     @Published var isAddingEvent: Bool = false
 
     let eventManager = EventManager()
 
     func loadDays() {
         do {
-            days = try DateManager.generateDatesForMonth()
+            days = try DateManager.generateDatesForMonth(for: dateForGenerating)
             if days.isEmpty {
                 throw CalendarError.emptyDatesArray
             }
+        } catch let error as CalendarError {
+            errorMessage = error.localizedDescription
+        } catch {
+            errorMessage = "Something went wrong"
+        }
+    }
+    
+    
+    func updateMonthAndYear() {
+        monthAndYear = DateManager.retrieveMonthAndYear(from: dateForGenerating)
+    }
+    
+    
+    func changeMonth(by value: Int) {
+        do {
+            guard let newDate =
+                    try DateManager.changeDateByMonth(by: value, from: dateForGenerating)
+            else {
+                throw CalendarError.invalidDate
+            }
+            
+            dateForGenerating = newDate
+            updateMonthAndYear()
+            loadDays()
+            
         } catch let error as CalendarError {
             errorMessage = error.localizedDescription
         } catch {
